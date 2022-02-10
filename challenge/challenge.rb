0691f07089d4e576c745fcf3ff7442d9dfae6423
Stirling Hostetter <stirhoss@icloud.com>
2022-02-09 21:21:24 -0600

    Add Paired challenge

---
+++ challenge/challenge.rb
@@ -0,0 +1,17 @@
+# declare number_of_books and set it equal to 200
+number_of_books = 200 
+# declare checked_out and set it equal to the boolean true
+checked_out = true
+# declare book_title and store a string in it
+book_title = "Gravity's Rainbow"
+# declare library_rules and store a string in it.
+library_rules = "quiet" 
+library_cards = 50
+library_open = true
+
+books_in_currently = ["The Remains of the Day", "Gravity's Rainbow", "For Whom the Bell Tolls"]
+
+# I chose challenge.rb because it is the ruby file for the challenge we are doing. 
+
+
+ 
---
+++ challenge/index.md
@@ -0,0 +1,7 @@
+# Book Titles
++ The Sun Also Rises
++ Gravity's Rainbow
++ The Remains of the Day
++ Atonement
++ Slaughterhouse 5 
+
